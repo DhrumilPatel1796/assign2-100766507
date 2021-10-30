ccf88df77876eac2ba8612393d5303ed4ee6fe80
Dhrumil <dhrumilcsk@gmail.com>
2021-10-30 17:43:34 -0400

    Creating template file and replacing h1 tag to p tag

---
+++ themes/twenty20one-child/template.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template Name: Template
+ * 
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty 1.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+
+    <p>This is a template file.</p>
+
+    <?php
+    // Start the loop
+    while ( have_posts() );
+    the_post();
+
+    // Include the page content template
+    get_template_part( 'cintent', 'page');
+
+    // If comments are open or we have at least one comment, load up the comment template.
+    if( comments_open() || get_comments_number() ):
+        comments_template();
+    endif;
+
+    // End the loop
+endwhile;
+?>
