acc7458c33b51642af85f18a5024fe1cd2d05bde
Dhrumil <dhrumilcsk@gmail.com>
2021-12-07 13:14:28 -0500

    Creating front page template file
    Added Front Page Template

---
+++ themes/twenty20one-child/front-page.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template Name: Front Page Template
+ *
+ * In order for this template to work a page needs to be created with the About template selected.
+ */
+/**
+ * @since Twenty Twenty One 1.0
+ */
+
+get_header();
+?>
+
+<main id="site-content" role="main">
+    
+	<?php
+
+	if ( have_posts() ) {
+
+		while ( have_posts() ) {
+			the_post();
+
+			get_template_part( 'template-parts/content/content-page');
+		}
+	}
+
+	?>
+
+</main><!-- #site-content -->
+
+<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+
+<?php get_footer(); ?>
